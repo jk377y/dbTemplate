4d7487e2e02ba3e8d7a5685b05b5f08b81a2b560
James Kelly <jk377y@gmail.com>
2023-02-20 15:13:52 -0600

    uploading boilerplate

---
+++ db/schema.sql
@@ -0,0 +1,3 @@
+-- Clears old database and creates a new one
+DROP DATABASE IF EXISTS mytech_db;
+CREATE DATABASE mytech_db;
---
+++ public/js/login.js
@@ -0,0 +1,39 @@
+const loginFormHandler = async (event) => {
+	event.preventDefault();
+	const email = document.querySelector('#email-login').value.trim();
+	const password = document.querySelector('#password-login').value.trim();
+	if (email && password) {
+		const response = await fetch('/api/users/login', {
+			method: 'POST',
+			body: JSON.stringify({ email, password }),
+			headers: { 'Content-Type': 'application/json' },
+		});
+		if (response.ok) {
+			document.location.replace('/api/**something here**');
+		} else {
+			alert('Failed to log in.');
+		}
+	}
+};
+
+const signupFormHandler = async (event) => {
+	event.preventDefault();
+	const username = document.querySelector('#userName-signup').value.trim();
+	const email = document.querySelector('#email-signup').value.trim();
+	const password = document.querySelector('#password-signup').value.trim();
+	if (username && email && password) {
+		const response = await fetch('/api/**something here**', {
+			method: 'POST',
+			body: JSON.stringify({ username, email, password }),
+			headers: { 'Content-Type': 'application/json' },
+		});
+		if (response.ok) {
+			document.location.replace('/api/**something here**');
+		} else {
+			alert('Failed to sign up.');
+		}
+	}
+};
+
+document.querySelector('#login').addEventListener('click', loginFormHandler);
+document.querySelector('#signUp').addEventListener('click', signupFormHandler);
